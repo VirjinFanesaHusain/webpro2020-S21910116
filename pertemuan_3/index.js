1033ad979dc4c7527984674adf8134c1aacb713f
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-08-27 05:33:30 +0800

    Latihan_pertemuan_o3

---
+++ pertemuan_3/index.js
@@ -0,0 +1,14 @@
+const firstName = "Virjin";
+let age = 19;
+age = 21;
+const isMerried = true;
+
+console.log(firstName);
+console.log(age);
+console.log(isMerried);
+
+console.log("My name is " + firstName + "i'm" + age + "years old");
+
+let bilangan = 10;
+console.log(bilangan == "10");
+console.log(bilangan === "10");
