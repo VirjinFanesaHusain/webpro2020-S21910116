ba4e8a21c88714fc155710c4f6e65f70e50e12f9
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-09-03 02:27:50 +0800

    Array lanjutan_02

--- pertemuan_5/index.js
+++ pertemuan_5/index.js
@@ -1,9 +1,10 @@
+/*deklarasi array menggunakan []
 let angka = [1,2,3,4,5,6,7,8];
     console.log(angka);
 let buah =["mangga","jeruk","apel"]
     console.log(buah);
 
-let arr = ['text',1,true];
+let arr = ["text",1,true];
 console.log(arr);
 
 let myFunc = function () {
@@ -13,6 +14,23 @@ let myFunc = function () {
 let arr2 =["text",1,true,myFunc(), [1,2,3,4,5]];
 //mengakses element dalam array
 console.log(arr2[2]);
-console.log(arr[3]);
+console.log(arr2[3]);
 console.log(arr2[4][2]);
 
+console.log(typeof arr2);
+console.log(arr2.length);
+
+//deklarasi array menggunakan kata kunci new
+
+let buah2 = new Array("durian","nagka","jambu");
+console.log(buah2);
+*/
+
+//Manipulasi Array
+//1.Menambah isi Array
+
+let mhs = []
+mhs[0] = "virgin";
+mhs[1] = "fanesa";
+mhs[2] = "nesa";
+console.log(mhs);
