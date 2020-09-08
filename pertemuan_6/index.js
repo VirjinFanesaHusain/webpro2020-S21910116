1ded5241c27e09e3fb87c85c4fb6b5bf4f023593
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-09-08 12:36:05 +0800

    Belajar Object

---
+++ pertemuan_6/index.js
@@ -0,0 +1,25 @@
+/*
+    object 
+*/
+
+//conts mahasiswa1 = ["john","doc",31,true];
+
+conts mhs1 = {
+    namaDepan: "Virgin",
+    namaBelakang: "Husain",
+    umur: 31,
+    sudahLulus: true,
+    alamat: {
+        jalan: "Jln.Arnold Mononutu",
+        kecamatan: "Airmadidi",
+        kabupaten: "Minahasa Utara",
+},
+IPsemester: [3.05, 3.25, 3.0, 3.4],
+hitungIPK: function (){
+    let total = 0;
+    },
+};
+
+//2. kata kunci new
+//const mhs2 = new object();
+//mhs2.namaDepan = "Fanesa" 
