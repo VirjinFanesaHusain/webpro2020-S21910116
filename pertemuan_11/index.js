964e89ff1afd0bb028b470336ea458a72b154819
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-09-29 12:04:44 +0800

    Exeercise_Pertemuan_11

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -34,7 +34,7 @@ function yearUntilRentirement(year, firstName) => {
   let age = 28;
   let retirement = 65 - age;
   if (retirement > 0) {
-    console.log( `${firstName} + " retired in " + retirement + " years"}`);
+    console.log( `${firstName + " retired in " + retirement + " years"}`);
   }
   else {
     console.log( `${firstName + " is already retired."}`);
