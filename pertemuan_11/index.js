30d79e9b2d6287dda8d74d8ab40f5cfd9aeb70d3
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-09-29 12:00:55 +0800

    Exercise_pertemuan 11

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -1,4 +1,4 @@
-// fungsi dengan parameter
+/* fungsi dengan parameter
 const operasiPenjumlahan = (bilangan1, bilangan2) => {
     const hasil =  bilangan1 + bilangan2;
     return hasil;
@@ -13,3 +13,30 @@ const operasiPenjumlahan = (bilangan1, bilangan2) => {
   }
   
   console.log(namaJenisAnjing());
+  */
+
+
+/*Exercise
+ function yearUntilRentirement(year, firstName) {
+  var age = 28;
+  var retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(firstName + " retired in " + retirement + " years");
+  }
+  else {
+    console.log(firstName + " is already retired.");
+  }
+}
+*/
+
+
+function yearUntilRentirement(year, firstName) => {
+  let age = 28;
+  let retirement = 65 - age;
+  if (retirement > 0) {
+    console.log( `${firstName} + " retired in " + retirement + " years"}`);
+  }
+  else {
+    console.log( `${firstName + " is already retired."}`);
+  }
+}
