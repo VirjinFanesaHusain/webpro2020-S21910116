28bbb7cacde807063ce4d548b941cb729ce24a02
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-10-12 21:24:34 +0800

    Membuat Module

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,12 @@
+// export const numbers = [1,2,3,4,5];
+
+// export const world = () => {
+//     console.log('Hello world');
+// }
+
+const hello = (user) => {
+    console.log{`Helo, ${user}`}
+}
+
+
+export default hello;
---
+++ pertemuan_16/index.js
@@ -0,0 +1,7 @@
+// // import { hello, world } from './hello.js'
+// import * as helper from './hello.js'
+import Func1 hello from './hello.js'
+
+Func1('Virgin Husain');
+// helper.world();
+// console.log(helper.numbers)
