fc34af64499c475ed91babe0db7f40edb92affec
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-10-14 17:47:15 +0800

    JavaScript Asynchronous

---
+++ pertemuan_17/index.js
@@ -0,0 +1,10 @@
+console.log('Hello');
+setTimeout(() => { console.log('Javascript')},100) // tunda selama 100 miliseconds
+console.log('Coder');
+
+/* ----------
+Output :
+Hello!
+Coder
+Javascipt
+------------*/
