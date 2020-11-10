fda3b830820872063ab4a1d54294f19fc2e2caba
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-11-10 09:02:56 +0800

    Basic-React

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import header from "./components/header";
-import hello from "./components/hello";
-import content from './components/content';
+import Header from "./components/header";
+import Hello from "./components/hello";
+import Content from "./components/content";
 
 
 function App() {
